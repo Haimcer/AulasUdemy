8effa50ee833639fc1b4496bd224242eaa8c3e21
Haimcer <allan1995.ars@gmail.com>
2022-09-27 11:36:18 -0300

    atualização

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,3 +1,4 @@
+import 'Post.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
@@ -11,41 +12,89 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Future<Map> _recuperarPreco() async {
-    String url = "https://blockchain.info/ticker";
-    http.Response response = await http.get(url);
-    return json.decode(response.body);
+  String _urlBase = "https://jsonplaceholder.typicode.com";
+
+  Future<List<Post>> _recuperarPostagem() async {
+    http.Response response = await http.get(_urlBase + "/posts");
+    var dadosJson = json.decode(response.body);
+
+    List<Post> postagens = [];
+
+    for (var post in dadosJson) {
+      print("post: " + post["title"]);
+      Post p = Post(post["userId"], post["body"], post["id"], post["title"]);
+      postagens.add(p);
+    }
+    return postagens;
   }
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Map>(
-      future: _recuperarPreco(),
-      builder: ((context, snapshot) {
-        String resultado = "";
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            break;
-          case ConnectionState.active:
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Consumo de serviço avançado"),
+      ),
+      body: FutureBuilder<List<Post>>(
+        future: _recuperarPostagem(),
+        builder: ((context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.active:
+              break;
 
-          case ConnectionState.waiting:
-            print("conexão waiting");
-            resultado = "Carregando...";
-            break;
-          case ConnectionState.done:
-            print("conexão done");
-            if (snapshot.hasError) {
-              resultado = "Erro ao carregar os dados.";
-            } else {
-              double valor = snapshot.data!["BRL"]["buy"];
-              resultado = "Preço do bitcoin ${valor.toString()}";
-            }
-            break;
-        }
-        return Center(
-          child: Text(resultado),
-        );
-      }),
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+              break;
+            case ConnectionState.done:
+              if (snapshot.hasError) {
+                print("Lista erro ao carregar");
+                return AlertDialog(
+                  title: Text("Erro ao carregar"),
+                  titlePadding: EdgeInsets.all(20),
+                  titleTextStyle: TextStyle(fontSize: 20, color: Colors.orange),
+                  content: Text("Fudeu"),
+                  actions: <Widget>[
+                    FloatingActionButton(
+                        onPressed: () {
+                          print("selecionado sim");
+                          Navigator.pop(context);
+                        },
+                        child: Text("sim")),
+                    FloatingActionButton(
+                        onPressed: () {
+                          print("selecionado não");
+                          Navigator.pop(context);
+                        },
+                        child: Text("Não")),
+                  ],
+                );
+              } else {
+                print("Lista carregou!!");
+                return ListView.builder(
+                    itemCount: snapshot.data?.length,
+                    itemBuilder: (context, index) {
+                      List<Post>? lista = snapshot.data;
+                      Post post = lista![index];
+                      return ListTile(
+                        title: Text(post.title),
+                        subtitle: Text(post.id.toString()),
+                      );
+                    });
+              }
+              break;
+          }
+          return AlertDialog(
+            title: Text("titulo"),
+            titlePadding: EdgeInsets.all(20),
+            titleTextStyle: TextStyle(fontSize: 20, color: Colors.orange),
+            content: Text("erro no build"),
+          );
+          ;
+        }),
+      ),
     );
   }
 }
---
+++ lib/Post.dart
@@ -0,0 +1,32 @@
+class Post {
+  int _userId;
+  int _id;
+  String _title;
+  String _body;
+
+  Post(this._userId, this._body, this._id, this._title);
+
+  String get body => _body;
+
+  set body(String value) {
+    _body = value;
+  }
+
+  String get title => _title;
+
+  set title(String value) {
+    _title = value;
+  }
+
+  int get id => _id;
+
+  set id(int value) {
+    _id = value;
+  }
+
+  int get userId => _userId;
+
+  set userId(int value) {
+    _userId = value;
+  }
+}
