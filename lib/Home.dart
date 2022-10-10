d4f84db7a317cd950c89eb38cd7117312ef2abe8
Haimcer <allan1995.ars@gmail.com>
2022-10-10 17:21:43 -0300

    final

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -10,8 +11,21 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   TextEditingController _controllerCampo = TextEditingController();
 
-  _Salvar() {}
-  _Recuperar() {}
+  _Salvar() async {
+    String valorDigitado = _controllerCampo.text;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString("nome", valorDigitado);
+  }
+
+  _Recuperar() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.getString("nome") ?? "sem valor";
+  }
+
+  _Remover() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove("nome");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -57,7 +71,22 @@ class _HomeState extends State<Home> {
                 style: ButtonStyle(
                     backgroundColor:
                         MaterialStatePropertyAll<Color>(Colors.blue)),
-              )
+              ),
+              Container(
+                padding: EdgeInsets.all(7),
+              ),
+              TextButton(
+                onPressed: _Remover,
+                child: Text(
+                  "Remover",
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll<Color>(Colors.blue)),
+              ),
             ],
           ),
         ]),
